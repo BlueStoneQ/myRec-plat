d38c1aad4aa1414f0db9ea24acf7d0d4ad1b1d06
乔阳 <2282907674@qq.com>
2019-09-26 15:30:11 +0800

    feat(webpack): 清除上次打包文件：clean-webpack-plugin

--- build/webpack.common.config.js
+++ build/webpack.common.config.js
@@ -2,7 +2,8 @@
  * webpack中dev和prod两种模式公用的配置
  */
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackCleanPlugin = require('webpack-clean-plugin');
 const config = require('./config');
 
 // 基础路径
@@ -14,7 +15,9 @@ module.exports = {
   // mode: 'development',
   context: path.resolve(__dirname, ROOT_PATH),
   entry: {
-    main: path.resolve(SRC_PATH, 'index.js')
+    main: path.resolve(SRC_PATH, 'index.js'),
+    app1: path.resolve(SRC_PATH, 'index.js'),
+    app2: path.resolve(SRC_PATH, 'index.js')
   },
   output: {
     path: BUILD_PATH,
@@ -23,6 +26,8 @@ module.exports = {
   module: {},
   plugins: [
     // https://github.com/jantimon/html-webpack-plugin#configuration
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(),
+    // https://www.npmjs.com/package/clean-webpack-plugin
+    new WebpackCleanPlugin() // 默认清除dist目录
   ]
 }

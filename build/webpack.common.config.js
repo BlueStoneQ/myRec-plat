f39fc4df33a4aa79705065d0981a96741e505bc5
BlueStoneQ <2282907674@qq.com>
2019-09-28 15:59:21 +0800

    feat(webpack): 利用url-loader + file-loader处理图片

--- build/webpack.common.config.js
+++ build/webpack.common.config.js
@@ -12,15 +12,28 @@ const SRC_PATH = config.SRC_PATH;
 const BUILD_PATH = config.BUILD_PATH;
 
 module.exports = {
-  mode: 'development',
+  // mode: 'development',
   context: path.resolve(__dirname, ROOT_PATH),
   entry: {
-    main: path.resolve(SRC_PATH, 'index.js'),
-    app2: path.resolve(SRC_PATH, 'a.js')
+    main: path.resolve(SRC_PATH, 'index.js')
   },
   output: {
     path: BUILD_PATH,
     filename: '[name].[hash:8].bundle.js'
+  },
+  resolve: {
+    alias: {
+      '$public': path.resolve(ROOT_PATH, 'public'),
+      '$src': path.resolve(SRC_PATH),
+      '$views': path.resolve(SRC_PATH, 'views'),
+      '$components': path.resolve(SRC_PATH, 'components'),
+      '$config': path.resolve(SRC_PATH, 'config'),
+      '$utils': path.resolve(SRC_PATH, 'utils'),
+      '$redux': path.resolve(SRC_PATH, 'redux'),
+      '$mock': path.resolve(SRC_PATH, 'mock'),
+      '$services': path.resolve(SRC_PATH, 'services')
+    },
+    extensions: [ '.js', '.jsx', '.json' ]
   },
   module: {
     rules: [
@@ -34,6 +47,19 @@ module.exports = {
         include: path.resolve(SRC_PATH),
         exclude: path.resolve(ROOT_PATH, 'node_modules'),
         use: ['style-loader', 'css-loader']
+      }, {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        include: path.resolve(ROOT_PATH, 'public', 'assets', 'images'),
+        exclude: path.resolve(ROOT_PATH, 'node_modules'),
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024 * 8, // 图片小于8kb 打包为base64
+              outputPath: 'images'
+            }
+          }
+        ]
       }
     ]
   },
@@ -41,14 +67,6 @@ module.exports = {
     // https://github.com/jantimon/html-webpack-plugin#configuration
     new HtmlWebpackPlugin({
       filename: 'index.html', // 输出html的名称
-      template: path.resolve(ROOT_PATH, 'public', 'index.ejs'),
-      templateParameters: {
-        title: 'myRec'
-      }
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['app2'],
-      filename: 'index2.html', // 输出html的名称
       template: path.resolve(ROOT_PATH, 'public', 'index.ejs'),
       templateParameters: {
         title: 'myRec'
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 /**
  * 根文件
  */
-const path = require(path);
-require('./index.css');
+import ipg from '$public/assets/images/heart.jpg';
+
 const test = () => {
+  const imgDom = document.createElement('img');
+  const rootDom = document.getElementById('root');
+  imgDom.src = ipg;
+  rootDom.appendChild(imgDom);
   console.log('12345678 start: ');
 }
 

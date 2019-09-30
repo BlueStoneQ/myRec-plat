24cad84c5ef103a49bd2d809182cf4fbbff5c1ce
BlueStoneQ <2282907674@qq.com>
2019-09-30 12:49:08 +0800

    feat(webpack): devServer支持同局域网移动端设备无线联调

--- build/devServer.js
+++ build/devServer.js
@@ -1,12 +1,21 @@
+/**
+ * 自定义本地服务器
+ * 1. 服务于开发阶段的调试
+ * 2. 支持热刷新（HMR）
+ * 3. 支持同局域网移动端设备无线联调（https://juejin.im/post/5d9181e0e51d4578414c439e）
+ */
 const webpack = require('webpack');
 const express = require('express');
 const WebpackDevMiddleWare = require('webpack-dev-middleware');
 const config = require('./config.js');
 const open = require('open');
+const chalk = require('chalk');
+const ip = require('ip');
 const webpackConfig = require('./webpack.dev.config.js');
 
 const PORT = config.PORT;
-const url = `http://localhost:${PORT}`;
+const URL = `http://localhost:${PORT}`;
+const ipURL = `http://${ip.address()}:${PORT}`;
 
 const compiler = webpack(webpackConfig);
 
@@ -18,7 +27,11 @@ const app = express();
 
 app.use(devMiddle);
 
-app.listen(PORT, () => { 
-  console.log('app listen on port 3002');
-  open(url);
+app.listen(PORT);
+
+// webpack编译完成后回调
+devMiddle.waitUntilValid(() => { 
+  console.log(chalk.cyan(`Local: ${URL}`));
+  console.log(chalk.cyan(`On Your Network: ${ipURL}`));
+  open(URL);
 });

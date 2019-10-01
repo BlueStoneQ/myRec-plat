686ff7f51898a89f0c7e684752dfa18354b98baa
BlueStoneQ <2282907674@qq.com>
2019-10-01 15:09:29 +0800

    fix(React): 在js中引入静态资源-图片

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -3,10 +3,20 @@
  */
 import React, { Component } from 'react'
 import './App.scss'
+import imgBk from '$public/assets/images/heart.jpg'
 
 class App extends Component {
   render () {
-    return <div className='app-wrap'>Hello word</div>
+    return (
+      <div
+        className='app-wrap'
+        style={{
+          background: `url(${imgBk})`
+        }}
+      >
+        123
+      </div>
+    )
   }
 }
 
